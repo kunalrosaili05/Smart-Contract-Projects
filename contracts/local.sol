ad4723f1cf45d84f7872e7c53f462a15fe9d27e9
kunalrosaili05 <kunalrosaili05@gmail.com>
2023-09-02 21:43:58 +0530

    upload code

---
+++ contracts/local.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.8.0 < 0.8.21;
+
+contract local
+{
+    uint public count;
+    
+
+constructor(uint new_count)
+{
+    count=new_count;
+}
+}
